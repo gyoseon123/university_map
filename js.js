3f8570b6ab3f5c8594ca422532e57af0863e3c86
gyoseon123 <gyoseon123@naver.com>
2022-11-10 03:18:08 +0900

    크롬드라이버 추가

--- js.js
+++ js.js
@@ -1,21 +1,11 @@
 
 var mapOptions = {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
+    center: new N.LatLng(37.3595704, 127.105399),
     zoom: 20
 }
 
-var map = new naver.maps.Map('map', {
-    center: new naver.maps.LatLng(37.5666805, 126.9784147),
-    zoom: 13,
-    minZoom: 6,
-    mapTypeId: naver.maps.MapTypeId.HYBRID,
-    zoomControl: true,
-    zoomControlOptions: {
-        position: naver.maps.Position.TOP_RIGHT
-    },
-    mapDataControl: false,
-    logoControlOptions: {
-        position: naver.maps.Position.LEFT_BOTTOM
-    },
-    disableKineticPan: false
+
+var marker = new naver.maps.Marker({
+    position: new naver.maps.LatLng(37.551563, 126.988306),
+    map: map
 });
--- score_info/score_scrap.py
+++ score_info/score_scrap.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup as bs
-
-
-page = ""
+driver = webdriver.Chrome()
+url = 'https://www.google.com'
+driver.get(url)
