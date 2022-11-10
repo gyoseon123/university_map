9ee119e0f6e96274c2a0fc4c8bab224c83d7bdca
gyoseon123 <gyoseon123@naver.com>
2022-11-11 03:04:05 +0900

    수시정보 스크래핑 - 완료

--- score_info/score_scrap.py
+++ score_info/score_scrap.py
@@ -1,4 +1,40 @@
 from selenium import webdriver
-driver = webdriver.Chrome()
-url = 'https://www.google.com'
+from bs4 import BeautifulSoup as bs
+
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("headless")
+driver = webdriver.Chrome('score_info\chromedriver.exe', options=options)
+url = 'https://ipsi.jinhak.or.kr/subList/20000000095'
 driver.get(url)
+driver.implicitly_wait(time_to_wait=1000)
+driver.find_element(by='id', value='pass').click()
+driver.find_element(by='id', value='srchbtn').click()
+
+
+
+
+school_score = {}
+
+
+def scrap_10_page():
+    for page_count in range(3,13):
+        element = driver.find_element(by='css selector', value=f'#list_area > ul > li:nth-child({page_count}) > a')
+        driver.execute_script("arguments[0].click();", element)
+        html = driver.page_source
+        soup = bs(html, "html.parser")
+        for i in range(1,16):
+            school_name = soup.select_one(f'#list_area > div > table > tbody > tr:nth-child({i}) > td:nth-child(4)').text
+            score_info = float(soup.select_one(f'#list_area > div > table > tbody > tr:nth-child({i}) > td:nth-child(11)').text)
+            try:
+                school_score[school_name][0] += 1
+                school_score[school_name][1] += score_info
+            except:
+                school_score[school_name] = [1, score_info]
+
+
+scrap_10_page()
+for school, score in school_score.items():
+    print(school+':'+str(round(score[1]/score[0], 2)))
+driver.quit()
