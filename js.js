3ef880709bdafdae8eebe0efb90b084bca1d621d
gyoseon123 <75434535+gyoseon123@users.noreply.github.com>
2023-08-21 04:43:26 +0000

    Update index.html and js.js

--- js.js
+++ js.js
@@ -9,3 +9,13 @@ var marker = new naver.maps.Marker({
     position: new naver.maps.LatLng(37.551563, 126.988306),
     map: map
 });
+
+
+var map = null;
+        
+function initMap() {
+    map = new N.Map('map', {
+        center: new N.LatLng(37.3595704, 127.105399),
+        zoom: 10
+    });
+}
